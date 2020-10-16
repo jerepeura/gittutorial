942a4e198c13d7ac861703331eec63e810b24ceb
Tomppa22 <tomi___00@hotmail.com>
2020-10-16 15:25:51 +0300

    mathtools

--- mathtools.py
+++ mathtools.py
@@ -1,5 +1,3 @@
-
-
 def isPrime(n):
     '''Returns true if a number n is a prime number'''
     if n == 2 or n == 3:
@@ -10,3 +8,12 @@ def isPrime(n):
         if n % i == 0:
             return False
     return True
+
+def geometric(a, ratio, n):
+    '''Calculates the sum of a geometric serie of n elements.
+       A geometric sequence is of the form: a, a*r, a*r*r, a*r*r*r,...
+       n is the number of elements in the sequence.'''
+    #Get the geometric sequence
+    sequence = [a*(ratio**x) for x in range(n)]
+    #Calculates its sum
+    return sum(sequence)
